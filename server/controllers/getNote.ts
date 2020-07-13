e7cf3304b7ed8e9cb3a210fa429f133ff662d90d
John Leonardo <johnleonardo@csus.edu>
2020-07-13 13:31:36 -0500

    getNote()

---
+++ server/controllers/getNote.ts
@@ -0,0 +1,23 @@
+import { Note } from '../helpers/connect.ts';
+
+// schemas
+import { NoteSchema } from '../models/Note.ts';
+
+// function for reading a note from db (given ID)
+export const getNote = async (ctx: any) => {
+  try {
+    // get id from request
+    const id: string = ctx.params.id;
+
+    // attempt to get note with given id
+    const note: NoteSchema = await Note.findOne({ _id: id });
+
+    // send notes back in response
+    ctx.response.body = note;
+  } catch (e) {
+    // when request fails
+    ctx.response.body = null;
+    ctx.response.status = 500;
+    console.log(e);
+  }
+};
