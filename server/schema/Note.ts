556897d0bc9757c74553c20dd76333a829074b76
John Leonardo <johnleonardo@csus.edu>
2020-07-13 12:57:16 -0500

    refactor

--- server/schema/Note.ts
+++
@@ -1,7 +0,0 @@
-export interface INote {
-  _id: { $oid: string };
-  title: string;
-  body: string;
-  timestamp: Date;
-  likes: number;
-}
--- server/schema/index.ts
+++
@@ -1,5 +0,0 @@
-// import all schemas in this root dir here
-import { INote } from './Note.ts';
-
-// export all schemas to the outside world
-export { INote };
